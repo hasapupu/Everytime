10eb2fc41f9ccc01d65e477cb02153ebb745cff4
cornL <dev.team.cornl@gmail.com>
2025-02-12 21:55:44 +0100

    bootup sequence fully done

--- scripts/gameManager.gd
+++ scripts/gameManager.gd
@@ -8,6 +8,8 @@ var playerButtonWriting := false
 var loadedSceneNoEnv: Node2D 
 var mainTextbox: RichTextLabel
 signal squiggle
+var plUINode: Node2D
+
 
 func playSfx(sfx: AudioStream):
     sfxManager.stream_paused = false
@@ -28,10 +30,25 @@ func startMainLoop():
         i.queue_free()
     var tempUI = playerUI.instantiate()
     loadedScene.add_child(tempUI)
-    mainTextbox = tempUI.get_node("mainTextBox")
+    plUINode = tempUI.get_node("backGround/stageSelectNode")
+    mainTextbox = tempUI.get_node("backGround/textBoxNode/mainTextBox")
     loadedSceneNoEnv = Node2D.new()
     loadedScene.add_child(loadedSceneNoEnv)
     loadScene("res://nodes/yScene.tscn")
+    await get_tree().create_timer(1).timeout
+    _print({"You're back?": 0.7,"What, was staring into the white void so boring you'd rather talk to me instead?": 0.7,"To be honest, I can't relate.": 1},load("res://sfx/yVoice.wav"),0.07,load("res://fonts/MARYJO__.ttf"))
 
 func _on_timer_timeout():
     squiggle.emit()
+
+func _print(inp: Dictionary,sfx: AudioStream, delay: float, ft:FontFile):
+    plUINode.visible = false
+    mainTextbox.get_parent().visible = true
+    var _writer = writer.new()
+    _writer.finished.connect(finishPrinting)
+    await _writer.writeTo(mainTextbox,inp,sfx,delay,ft,self)
+
+func finishPrinting():
+    plUINode.visible = true
+    mainTextbox.text = ""
+    mainTextbox.get_parent().visible = false
