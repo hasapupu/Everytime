94e7ab312ab439ad9f4a7277853c51eaf4aa1e88
cornL <dev.team.cornl@gmail.com>
2025-04-08 16:06:20 +0200

    fixed audio bug

--- scripts/endButton.gd
+++ scripts/endButton.gd
@@ -4,6 +4,7 @@ var daddy: daddyMaster = get_tree().root.get_node("gameManager")
 
 func _on_button_down():
 	disabled = true
+	daddy.playMusic(load("res://music/new memory.wav"))
 	daddy.executeEventFunctions(
 		event.new(0,{},null,0,null,[load("res://backgrounds/yDefaultReach1.png"),load("res://backgrounds/yDefaultReach2.png")],"","",[],
 			event.new(0,{"It worked!":1,"Finally, a true paradise!":2,"And finally, it's been confirmed that you ARE good!":2,"Now I know...":2,"You'll never leave me!":2},load("res://sfx/yVoice.wav"),0.07,load("res://fonts/yFont.ttf"),[],"","",[],
